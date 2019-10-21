558bd221f39b5c47020cf5bee5cf3e1f996e4b44
Sanaa Syed <sanaasyed@Sanaas-MacBook-Pro.local>
2019-10-20 21:39:34 -0400

    latest

--- help.rb
+++ help.rb
@@ -5,7 +5,7 @@ require 'tty-screen'
 :green #1DB954
 :black #000000
 
-box2 = TTY::Box.frame(align: :center, width: TTY::Screen.width - 5, height: 11, title: {top_left: '   HELP   '}, border: :thick, padding: 1, style: {
+helpBox = TTY::Box.frame(align: :center, width: TTY::Screen.width - 5, height: 11, title: {top_left: '   HELP   '}, border: :thick, padding: 1, style: {
     fg: :green,
     border: {
       fg: :black,
@@ -16,4 +16,4 @@ box2 = TTY::Box.frame(align: :center, width: TTY::Screen.width - 5, height: 11, 
     The timer starts now! Using the four options given, guess the song!"
 end
 
-print box2
+print helpBox
