9ebf6ad954ed332b07aa09f2ea5c8b82de242ea7
Sanaa Syed <sanaasyed@Sanaas-MacBook-Pro.local>
2019-10-24 13:13:37 -0400

    implemented changes

--- help.rb
+++ help.rb
@@ -2,18 +2,23 @@ require 'rspotify'
 require 'tty-box'
 require 'tty-screen'
 
-:green #1DB954
-:black #000000
+module HelpBox
 
-helpBox = TTY::Box.frame(align: :center, width: TTY::Screen.width - 5, height: 11, title: {top_left: '   HELP   '}, border: :thick, padding: 1, style: {
-    fg: :green,
-    border: {
-      fg: :black,
-      bg: :green
-    }
-  }) do 
-    "SCHPOTIFY INSTRUCTIONS\nObjective: Guess the song!\n1. Search for any playlist that is on Spotify.\n2. A random song from that playlist will start playing.\n3. 
-    The timer starts now! Using the four options given, guess the song!"
-end
+  def self.printHelpBox()
+    :green #1DB954
+    :black #000000
 
-print helpBox
+    helpBox = TTY::Box.frame(align: :center, width: TTY::Screen.width - 5, height: 11, title: {top_left: '   HELP   '}, border: :thick, padding: 1, style: {
+        fg: :green,
+        border: {
+          fg: :black,
+          bg: :green
+        }
+      }) do 
+        "SCHPOTIFY INSTRUCTIONS\nObjective: Guess the song!\n1. Search for any playlist that is on Spotify.\n2. A random song from that playlist will start playing.\n3. 
+        The timer starts now! Using the four options given, guess the song!"
+    end
+
+  end
+
+end 
